e41ecfaf150fb2f16d261a1753e66bdfda7add53
Leon <chin340823@163.com>
2018-09-05 09:22:33 +0800

    添加weather接口

---
+++ utils/weather.py
@@ -0,0 +1,32 @@
+import requests
+import inspect
+import sys
+
+class AliWeather:
+    
+    def __init__(self):
+        self.weather_url = 'https://jisutqybmf.market.alicloudapi.com/weather/query'
+        self.method = 'GET'
+        self.appcode = '397328e6ac4e4e1bbfa44afd2f37ee0d'
+
+
+    def query(self, **kwargs):
+        '''
+        @param city: STRING,可选    城市（city,cityid,citycode三者任选其一）
+        @param citycode: STRING,可选    城市天气代号（city,cityid,citycode三者任选其一）
+        @param cityid: STRING,可选    城市ID（city,cityid,citycode三者任选其一）
+        @param ip: STRING,可选    IP
+        @param location: STRING,可选    经纬度 纬度在前,分割 如：39.983424,116.322987
+        '''
+        params = kwargs
+        headers = {'Authorization': 'APPCODE ' + self.appcode}
+        req = requests.get(self.weather_url, params, headers=headers)
+        if req.ok:
+            return req.json()
+        return None
+
+
+if __name__ == "__main__":
+    weather = AliWeather()
+    res = weather.query(city="杭州")
+    print(res)
